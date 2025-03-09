8ea7aae6df11533dde57efbe1f869a1f38b259a5
Zira3l137 <raulbairamov2002@gmail.com>
2025-03-09 18:34:34 +0400

    minor fixes

--- include/utils.h
+++ include/utils.h
@@ -10,7 +10,7 @@ namespace str {
 bool startswith(const std::string &haystack, const std::string &needle);
 bool endswith(const std::string &haystack, const std::string &needle);
 bool contains(const std::string &haystack, const std::string &needle);
-bool isallnum(const std::string &s);
+bool isNumber(const std::string &s);
 std::string toLower(const std::string &s);
 std::vector<std::string> *split(const std::string &s, char delim);
 
